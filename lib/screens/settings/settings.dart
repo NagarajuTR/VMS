a97f5891125fe8f891c916f04442af8a0da4cba5
NagarajuTR <nagarajutr105@gmail.com>
2024-06-09 19:46:28 +0530

    Added gradient bg.

--- lib/screens/settings/settings.dart
+++ lib/screens/settings/settings.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:visitors_management/const/colors_const.dart';
 import 'package:visitors_management/screens/custom_widget/custom_text.dart';
+import 'package:visitors_management/screens/custom_widget/gradient_background.dart';
 import 'package:visitors_management/screens/home/main_screen.dart';
 
 class Settings extends StatefulWidget {
@@ -23,49 +24,52 @@ class _Dashboard extends State<Settings> {
           backgroundColor: ColorsConst.instance.blue,
           centerTitle: true,
         ),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Card(
-                  child: Padding(
+        child: GradientBackground(
+          colors: const [Color(0xFF3333ff), Color(0xFFe6e6e6)],
+          child: Column(
+            children: [
+              Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    CustomText(
-                      text: 'My profile',
-                      textSize: ConstSize.instance.text24,
-                    ),
-                    const SizedBox(height: 5),
-                    Row(
-                      children: [
-                        Image.asset(
-                          'assets/png/profile.png',
-                          height: 100,
-                          width: 100,
-                        ),
-                        const SizedBox(width: 5),
-                        Wrap(
-                          direction: Axis.vertical,
-                          children: [
-                            CustomText(
-                              text: 'Arun yadav',
-                              textSize: ConstSize.instance.text24,
-                            ),
-                            CustomText(
-                              text: 'arunyadav@gmail.com',
-                              textSize: ConstSize.instance.textLarge,
-                            )
-                          ],
-                        )
-                      ],
-                    ),
-                  ],
-                ),
-              )),
-            )
-          ],
+                child: Card(
+                    child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      CustomText(
+                        text: 'My profile',
+                        textSize: ConstSize.instance.text24,
+                      ),
+                      const SizedBox(height: 5),
+                      Row(
+                        children: [
+                          Image.asset(
+                            'assets/png/profile.png',
+                            height: 100,
+                            width: 100,
+                          ),
+                          const SizedBox(width: 5),
+                          Wrap(
+                            direction: Axis.vertical,
+                            children: [
+                              CustomText(
+                                text: 'Arun yadav',
+                                textSize: ConstSize.instance.text24,
+                              ),
+                              CustomText(
+                                text: 'arunyadav@gmail.com',
+                                textSize: ConstSize.instance.textLarge,
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                )),
+              )
+            ],
+          ),
         ));
   }
 }
