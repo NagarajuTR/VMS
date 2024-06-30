7bb3961d25f15f3b8d4085ebcbc3112070ac43cf
NagarajuTR <nagarajutr105@gmail.com>
2024-06-30 22:32:47 +0530

    On pop navigate to dashboard impl.

--- lib/screens/employees/employees_home.dart
+++ lib/screens/employees/employees_home.dart
@@ -3,6 +3,7 @@ import 'package:visitors_management/const/colors_const.dart';
 import 'package:visitors_management/screens/custom_widget/custom_grdient_button.dart';
 import 'package:visitors_management/screens/custom_widget/custom_text.dart';
 import 'package:visitors_management/screens/custom_widget/gradient_background.dart';
+import 'package:visitors_management/screens/dashboard/dashboard.dart';
 import 'package:visitors_management/screens/employees/add_employee.dart';
 import 'package:visitors_management/screens/employees/employees.dart';
 import 'package:visitors_management/screens/home/main_screen.dart';
@@ -17,100 +18,111 @@ class EmployeesHome extends StatefulWidget {
 class _Dashboard extends State<EmployeesHome> {
   @override
   Widget build(BuildContext context) {
-    return MainScreen(
-      appBar: AppBar(
-        title: const Text(
-          "Employees",
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+    return WillPopScope(
+      onWillPop: () {
+        Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(builder: (context) => const Dashboard()),
+          (Route<dynamic> route) => false,
+        );
+        return Future.value(false);
+      },
+      child: MainScreen(
+        appBar: AppBar(
+          title: const Text(
+            "Employees",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          automaticallyImplyLeading: false,
+          backgroundColor: ColorsConst.instance.blue,
+          centerTitle: true,
         ),
-        automaticallyImplyLeading: false,
-        backgroundColor: ColorsConst.instance.blue,
-        centerTitle: true,
-      ),
-      child: GradientBackground(
-        colors: const [Color(0xFF3333ff), Color(0xFFe6e6e6)],
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 6),
-          child: Column(
-            children: [
-              Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      CustomText(
-                        text: "ADD",
-                        fontWeight: FontWeight.bold,
-                        textSize: ConstSize.instance.textLarge,
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      const CustomText(text: "Add new employee"),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      CustomGradientButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const AddEmployee(),
-                            ),
-                          );
-                        },
-                        child: const CustomText(
-                          text: "ADD NEW EMPLOYEE",
-                          color: Colors.white,
-                          fontWeight: FontWeight.w500,
+        child: GradientBackground(
+          colors: const [Color(0xFF3333ff), Color(0xFFe6e6e6)],
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 6),
+            child: Column(
+              children: [
+                Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        CustomText(
+                          text: "ADD",
+                          fontWeight: FontWeight.bold,
+                          textSize: ConstSize.instance.textLarge,
                         ),
-                      )
-                    ],
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        const CustomText(text: "Add new employee"),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        CustomGradientButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const AddEmployee(),
+                              ),
+                            );
+                          },
+                          child: const CustomText(
+                            text: "ADD NEW EMPLOYEE",
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 5,
-              ),
-              Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      CustomText(
-                        text: "VIEW ALL EMPLOYEES",
-                        fontWeight: FontWeight.bold,
-                        textSize: ConstSize.instance.textLarge,
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      const CustomText(text: "View total number of employees"),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      CustomGradientButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const Employees(),
-                            ),
-                          );
-                        },
-                        child: const CustomText(
-                          text: "VIEW EMPLOYEES",
-                          color: Colors.white,
-                          fontWeight: FontWeight.w500,
+                const SizedBox(
+                  height: 5,
+                ),
+                Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        CustomText(
+                          text: "VIEW ALL EMPLOYEES",
+                          fontWeight: FontWeight.bold,
+                          textSize: ConstSize.instance.textLarge,
                         ),
-                      )
-                    ],
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        const CustomText(
+                            text: "View total number of employees"),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        CustomGradientButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const Employees(),
+                              ),
+                            );
+                          },
+                          child: const CustomText(
+                            text: "VIEW EMPLOYEES",
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
--- lib/screens/settings/settings.dart
+++ lib/screens/settings/settings.dart
@@ -6,6 +6,7 @@ import 'package:visitors_management/model/employee.dart';
 import 'package:visitors_management/screens/auth/login.dart';
 import 'package:visitors_management/screens/custom_widget/custom_text.dart';
 import 'package:visitors_management/screens/custom_widget/gradient_background.dart';
+import 'package:visitors_management/screens/dashboard/dashboard.dart';
 import 'package:visitors_management/screens/home/main_screen.dart';
 
 class Settings extends StatefulWidget {
@@ -18,118 +19,128 @@ class Settings extends StatefulWidget {
 class _Dashboard extends State<Settings> {
   @override
   Widget build(BuildContext context) {
-    return MainScreen(
-      appBar: AppBar(
-        title: const Text(
-          "Settings",
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+    return WillPopScope(
+      onWillPop: () {
+        Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(builder: (context) => const Dashboard()),
+          (Route<dynamic> route) => false,
+        );
+        return Future.value(false);
+      },
+      child: MainScreen(
+        appBar: AppBar(
+          title: const Text(
+            "Settings",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          automaticallyImplyLeading: false,
+          backgroundColor: ColorsConst.instance.blue,
+          centerTitle: true,
         ),
-        automaticallyImplyLeading: false,
-        backgroundColor: ColorsConst.instance.blue,
-        centerTitle: true,
-      ),
-      child: GradientBackground(
-        colors: const [Color(0xFF3333ff), Color(0xFFe6e6e6)],
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Card(
-                  child: Padding(
+        child: GradientBackground(
+          colors: const [Color(0xFF3333ff), Color(0xFFe6e6e6)],
+          child: Column(
+            children: [
+              Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    CustomText(
-                      text: 'My profile',
-                      textSize: ConstSize.instance.text24,
-                    ),
-                    const SizedBox(height: 5),
-                    Row(
-                      children: [
-                        // Image.asset(
-                        //   'assets/png/profile.png',
-                        //   height: 100,
-                        //   width: 100,
-                        // ),
-
-                        CachedNetworkImage(
-                          imageUrl:
-                              Employee.instance.employee['imageUrl'] ?? "",
-                          imageBuilder: (context, imageProvider) => Container(
-                            width: 80.0,
-                            height: 80.0,
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              image: DecorationImage(
-                                  image: imageProvider, fit: BoxFit.cover),
-                            ),
-                          ),
-                          progressIndicatorBuilder:
-                              (context, url, downloadProgress) =>
-                                  CircularProgressIndicator(
-                                      value: downloadProgress.progress),
-                          errorWidget: (context, url, error) => const Icon(
-                            Icons.error,
-                            size: 80,
-                            color: Colors.blueGrey,
-                          ),
-                        ),
-                        const SizedBox(width: 5),
-                        Wrap(
-                          direction: Axis.vertical,
-                          children: [
-                            CustomText(
-                              text:
-                                  '${Employee.instance.employee['name'] ?? ''}',
-                              textSize: ConstSize.instance.text24,
-                            ),
-                            CustomText(
-                              text: '${Employee.instance.employee['email']}',
-                              textSize: ConstSize.instance.textLarge,
-                            )
-                          ],
-                        )
-                      ],
-                    ),
-                  ],
-                ),
-              )),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Card(
-                  child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    ListTile(
-                      onTap: () async {
-                        final SharedPreferences prefs =
-                            await SharedPreferences.getInstance();
-                        await prefs.clear();
-
-                        if (!mounted) return;
-
-                        Navigator.pushAndRemoveUntil(
-                            context,
-                            MaterialPageRoute(
-                                builder: (BuildContext context) =>
-                                    const Login()),
-                            ModalRoute.withName('/'));
-                      },
-                      leading: const Icon(Icons.logout),
-                      title: CustomText(
-                        text: "Logout",
-                        textSize: ConstSize.instance.textLarge,
+                child: Card(
+                    child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      CustomText(
+                        text: 'My profile',
+                        textSize: ConstSize.instance.text24,
                       ),
-                    )
-                  ],
-                ),
-              )),
-            ),
-          ],
+                      const SizedBox(height: 5),
+                      Row(
+                        children: [
+                          // Image.asset(
+                          //   'assets/png/profile.png',
+                          //   height: 100,
+                          //   width: 100,
+                          // ),
+
+                          CachedNetworkImage(
+                            imageUrl:
+                                Employee.instance.employee['imageUrl'] ?? "",
+                            imageBuilder: (context, imageProvider) => Container(
+                              width: 80.0,
+                              height: 80.0,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                image: DecorationImage(
+                                    image: imageProvider, fit: BoxFit.cover),
+                              ),
+                            ),
+                            progressIndicatorBuilder:
+                                (context, url, downloadProgress) =>
+                                    CircularProgressIndicator(
+                                        value: downloadProgress.progress),
+                            errorWidget: (context, url, error) => const Icon(
+                              Icons.error,
+                              size: 80,
+                              color: Colors.blueGrey,
+                            ),
+                          ),
+                          const SizedBox(width: 5),
+                          Wrap(
+                            direction: Axis.vertical,
+                            children: [
+                              CustomText(
+                                text:
+                                    '${Employee.instance.employee['name'] ?? ''}',
+                                textSize: ConstSize.instance.text24,
+                              ),
+                              CustomText(
+                                text: '${Employee.instance.employee['email']}',
+                                textSize: ConstSize.instance.textLarge,
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                )),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Card(
+                    child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ListTile(
+                        onTap: () async {
+                          final SharedPreferences prefs =
+                              await SharedPreferences.getInstance();
+                          await prefs.clear();
+
+                          if (!mounted) return;
+
+                          Navigator.pushAndRemoveUntil(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (BuildContext context) =>
+                                      const Login()),
+                              ModalRoute.withName('/'));
+                        },
+                        leading: const Icon(Icons.logout),
+                        title: CustomText(
+                          text: "Logout",
+                          textSize: ConstSize.instance.textLarge,
+                        ),
+                      )
+                    ],
+                  ),
+                )),
+              ),
+            ],
+          ),
         ),
       ),
     );
